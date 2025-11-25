cc5c47d425065dbef7367a7aa004e4bb7aa77447
Tyuiu.PisarevMA@outlook.com <frisssn_da@mail.ru>
2025-11-25 19:25:16 +0500

    Task6

---
+++ Tyuiu.PisarevMA.Sprint5.Task6.V14.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using System.IO;
+using Tyuiu.PisarevMA.Sprint5.Task6.V14.Lib;
+namespace Tyuiu.PisarevMA.Sprint5.Task6.V14.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckedExistsFile()
+        {
+            string path = @"C:\DataSprint5\InPutDataFileTask6V14.txt";
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+            Assert.AreEqual(true, fileExists);
+        }
+    }
+}
---
+++ Tyuiu.PisarevMA.Sprint5.Task6.V14/Program.cs
@@ -0,0 +1,31 @@
+﻿using Tyuiu.PisarevMA.Sprint5.Task6.V14.Lib;
+DataService ds = new DataService();
+Console.Title = "Спринт 5 | Выполнил: Писарев М. А. | АСОиУб-25-1";
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* Спринт #5                                                               *");
+Console.WriteLine("* Тема: Чтение набора данных из текстового файла                          *");
+Console.WriteLine("* Задание #6                                                              *");
+Console.WriteLine("* Вариант #14                                                             *");
+Console.WriteLine("* Выполнил: Писарев Максим Алексеевич | АСОиУб-25-1                       *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* УСЛОВИЕ:                                                                *");
+Console.WriteLine("* Дан файл в котором есть набор символьных данных. Найти количество       *");
+Console.WriteLine("* знаков препинания в заданной строке.                                    *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+Console.WriteLine("***************************************************************************");
+
+string path = @"C:\DataSprint5\InPutDataFileTask6V14.txt";
+
+Console.WriteLine($"Данные находятся в файле: {path}");
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+Console.WriteLine("***************************************************************************");
+
+double res = ds.LoadFromDataFile(path);
+
+Console.WriteLine(res);
+
+Console.ReadKey();
+
